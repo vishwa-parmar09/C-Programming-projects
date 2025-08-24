8252d90551f8164fd93adc0ba49b83ab8b0e0d5a
vishwa parmar <vishparmar04@gmail.com>
2025-08-24 16:27:53 +0530

    ledder if else

---
+++ max.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int n1,n2,n3;
+    printf("Enter 1st number : ");
+    scanf("%d",&n1);
+
+    printf("Enter 2nd number : ");
+    scanf("%d",&n2);
+
+    printf("Enter 3rd number : ");
+    scanf("%d",&n3);
+
+    if(n1>n2 && n1>n3){
+        printf("the max number is %d",n1);
+    }
+    else if(n2>n1 && n2>n3){
+        printf("the max number is %d",n2);
+    }
+    else{
+        printf("the max number is %d",n3);
+    }
+    return 0;
+}
